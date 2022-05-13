01df528dc9baa97e8bc6afc86f044fa5a94ee346
Timothy Agozie <GIG@Timothy-Agozie.local>
2022-05-13 12:41:54 +0000

    Minor edits

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
       <footer>
         <div className="nav-grid body-content">
           <div className="nav-grid-item">
-            <span className="f-txt">© {year} Timothy Agozie | All rights reserved.</span>
+            <span className="f-txt">© {year} Timothy Agozie &nbsp;|&nbsp; All rights reserved.</span>
           </div>
           <div className="">
             <span className="f-txt love" style={{float: "right"}}>..with love from <span aria-label="Ghana flag" role="img">🇬🇭</span></span>
